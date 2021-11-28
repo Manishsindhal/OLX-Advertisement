e6714044aa2e33f56ace0d4e38cb26de207a7219
Manish Sindhal <sindhalmanish@Gmail.com>
2021-11-29 00:48:19 +0530

    Push OLX-Masterdata DB

---
+++ olxmasterdatadb/tb_category.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM olxmasterdatadb.categories
+LIMIT 0, 1000
+
+-- Date: 2021-11-29 00:38
+*/
+INSERT INTO `` (`id`,`name`,`description`) VALUES (1,'Furniture','All Type of Furniture');
+INSERT INTO `` (`id`,`name`,`description`) VALUES (2,'Cars','All Type of Cars');
+INSERT INTO `` (`id`,`name`,`description`) VALUES (3,'Mobiles','All Type of Mobiles');
+INSERT INTO `` (`id`,`name`,`description`) VALUES (4,'Real Estate','All Type of RE');
+INSERT INTO `` (`id`,`name`,`description`) VALUES (5,'Sports','All Type of Sports items');
---
+++ olxmasterdatadb/tb_status.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM olxmasterdatadb.adv_status
+LIMIT 0, 1000
+
+-- Date: 2021-11-29 00:36
+*/
+INSERT INTO `` (`id`,`status`) VALUES (1,'OPEN');
+INSERT INTO `` (`id`,`status`) VALUES (2,'CLOSED');
