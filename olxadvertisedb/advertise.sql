0d0c2eb67b04764541f529e3ce1b4e54bb34b68e
Manish Sindhal <sindhalmanish@Gmail.com>
2021-11-29 00:49:07 +0530

    Push OLX-Advertise DB

---
+++ olxadvertisedb/advertise.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM olxadvertisedb.tb_advertise
+LIMIT 0, 1000
+
+-- Date: 2021-11-29 00:41
+*/
+INSERT INTO `` (`id`,`active`,`category`,`created_date`,`description`,`modified_date`,`posted_by`,`price`,`status`,`title`,`username`) VALUES (19,1,1,'2021-11-10','All type of furniture like sofa, table etc','2021-11-10','manish',20000.00,1,'Furnitue Sale','manish');
+INSERT INTO `` (`id`,`active`,`category`,`created_date`,`description`,`modified_date`,`posted_by`,`price`,`status`,`title`,`username`) VALUES (20,1,2,'2021-11-10','Car available for sale in india.','2021-11-15','manish',70000.00,1,'Car Sale','manish');
+INSERT INTO `` (`id`,`active`,`category`,`created_date`,`description`,`modified_date`,`posted_by`,`price`,`status`,`title`,`username`) VALUES (21,1,5,'2021-11-15','All Sports item for sale in india.','2021-11-15','anand',5000.00,1,'Sports Sale','anand');
+INSERT INTO `` (`id`,`active`,`category`,`created_date`,`description`,`modified_date`,`posted_by`,`price`,`status`,`title`,`username`) VALUES (22,1,3,'2021-11-18','All Type of Electronics item for sale in india.','2021-11-18','anand',5000.00,1,'Electronics Sale','anand');
+INSERT INTO `` (`id`,`active`,`category`,`created_date`,`description`,`modified_date`,`posted_by`,`price`,`status`,`title`,`username`) VALUES (23,1,3,'2021-11-26','All Type of Electronics item for sale in india.','2021-11-26','anand',12000.00,1,'Mobile Sale','anand');
+INSERT INTO `` (`id`,`active`,`category`,`created_date`,`description`,`modified_date`,`posted_by`,`price`,`status`,`title`,`username`) VALUES (24,1,4,'2021-11-26','E-Markiting','2021-11-26','manish',50000.00,1,'Real Estate Markiting','manish');
