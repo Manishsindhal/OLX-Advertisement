9970a68d38bbba8a547413fcf958dc44d85ae0a2
Manish Sindhal <sindhalmanish@Gmail.com>
2021-11-29 00:47:13 +0530

    Add file via upload
    Push OLX-User module db

---
+++ olxuserdb/users.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM olxuserdb.users
+LIMIT 0, 1000
+
+-- Date: 2021-11-29 00:23
+*/
+INSERT INTO `` (`id`,`password`,`roles`,`username`,`active`,`firstname`,`lastname`,`roless`) VALUES (28,'man123','ROLE_USER','man',1,'Manish','Sindhal',NULL);
+INSERT INTO `` (`id`,`password`,`roles`,`username`,`active`,`firstname`,`lastname`,`roless`) VALUES (29,'tom123','ROLE_USER','tom',1,'Tom','Cruise',NULL);
+INSERT INTO `` (`id`,`password`,`roles`,`username`,`active`,`firstname`,`lastname`,`roless`) VALUES (30,'anand123','ROLE_ADMIN','anand',1,'Anand','Kulkarni',NULL);
